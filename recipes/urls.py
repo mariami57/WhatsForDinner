e6ac954976565de6d1d9a468b7656e12a71691e0
mariami57 <mmitova3@gmail.com>
2025-09-28 15:53:08 +0300

    added some views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from recipes.views import RecipeCreateView
+from recipes.views import RecipeCreateView, RecipesListView
 
 urlpatterns = [
     path('create-recipe/', RecipeCreateView.as_view(), name='create-recipe'),
+    path('all-recipes/', RecipesListView.as_view(), name='all-recipes'),
+
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from recipes.forms import RecipeCreateForm
 from recipes.models import Recipe
@@ -12,3 +12,7 @@ class RecipeCreateView(CreateView):
     success_url = reverse_lazy('home')
     template_name = 'recipes/create-recipe.html'
 
+
+class RecipesListView(ListView):
+    model = Recipe
+    template_name = 'recipes/all-recipes.html'
