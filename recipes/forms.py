889953869dad9fb306683c46c6a18c4a4ee2254c
mariami57 <mmitova3@gmail.com>
2025-09-30 20:39:45 +0300

    updated recipe model

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,20 +6,29 @@ from recipes.models import Recipe
 class RecipeBaseForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = '__all__'
+        fields =  ['title', 'prep_time', 'cook_time', 'instructions', 'ingredients']
 
         labels = {
             'title': 'Recipe title:',
             'image': 'Upload Image:',
             'instructions': 'Instructions:',
             'ingredients': 'Ingredients:',
+            'prep_time':'Prep time:',
+            'cook_time': 'Cook time',
         }
 
         widgets = {
-            'instructions': forms.Textarea(attrs={'rows': 5,
-                'cols': 40, 'placeholder': "Add instructions here"}),
-            'ingredients': forms.Textarea(attrs={'rows': 5,
-                'cols': 40,'placeholder': "One ingredient per line:\nSugar\nFlour\nMilk"})
+            'instructions': forms.Textarea(attrs={'rows': 3,
+                'cols': 50}),
+            'ingredients': forms.Textarea(attrs={'rows': 3,
+                'cols': 50}),
+        }
+
+        placeholder= {
+            'instructions':'Add instructions here',
+            'ingredients': 'One ingredient per line:\nSugar\nFlour\nMilk',
+            'prep_time': 'Preparation time in minutes:',
+            'cook_time': 'Cooking time in minutes'
         }
 
 class RecipeCreateForm(RecipeBaseForm):
--- recipes/models.py
+++ recipes/models.py
@@ -8,6 +8,13 @@ class Recipe(models.Model):
     image = models.ImageField(upload_to='recipes',  blank=True, null=True)
     instructions = models.TextField(max_length=1000)
     ingredients = models.TextField(max_length=1000)
+    prep_time = models.PositiveIntegerField()
+    cook_time = models.PositiveIntegerField()
+
+    @property
+    def total_time(self):
+        return self.prep_time + self.cook_time
+
 
     def __str__(self):
         return self.title
