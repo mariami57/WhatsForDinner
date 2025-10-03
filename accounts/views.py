baf509f21c32f6292edfabd3fe6065508adff0b4
mariami57 <mmitova3@gmail.com>
2025-10-03 21:27:33 +0300

    made more updates on the templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,17 @@
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import WebUserCreationForm
-from accounts.models import WebUser
 
 
+
+UserModel = get_user_model()
 # Create your views here.
 class RegisterView(CreateView):
-    model = WebUser
+    model = UserModel
     form_class=WebUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/register.html'
@@ -18,4 +20,7 @@ class RegisterView(CreateView):
     def form_valid(self, form):
         messages.success(self.request, 'You have successfully registered! Please log in.')
         return super().form_valid(form)
+
+class ProfileDetails(DetailView):
+    model = UserModel
 
