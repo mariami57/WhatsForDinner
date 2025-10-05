ebf8884aa23df36b3245694ee1b90dd83feb386f
mariami57 <mmitova3@gmail.com>
2025-10-05 17:58:48 +0300

    updated globalsearchapiview

--- common/views.py
+++ common/views.py
@@ -1,5 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from WhatsForDinner.serializers import RecipeSerializer
+from recipes.models import Recipe
 
 
 # Create your views here.
@@ -12,3 +18,25 @@ class AboutPageView(TemplateView):
 class ContactsPageView(TemplateView):
     template_name = 'common/contacts.html'
 
+class GlobalSearchAPIView(APIView):
+    def get(self,request):
+        query = request.GET.get('q', '')
+        results = []
+
+        if query:
+            recipes = Recipe.objects.filter(
+                Q(user__username__icontains=query) |
+                Q(title__icontains=query) |
+                Q(ingredients__icontains=query) |
+                Q(total_time__icontains=query)
+            )
+
+            recipe_data = RecipeSerializer(recipes, many=True).data
+            for item in recipe_data:
+                item['label1'] = item['title']
+                item['label2'] = item['username']
+                item['label3'] = item['ingredients']
+                item['label4'] = item['total_time']
+            results.extend(recipe_data)
+
+        return Response(results)
