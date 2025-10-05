e6cd9a97116decd0223dc060062ddb7bdf090f7c
mariami57 <mmitova3@gmail.com>
2025-10-05 19:39:47 +0300

    added search bar for time

--- common/urls.py
+++ common/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from common.views import HomeView, AboutPageView, ContactsPageView, GlobalSearchAPIView
+from common.views import HomeView, AboutPageView, ContactsPageView, GlobalSearchAPIView, TimeSearchAPIView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
 
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
-    path('api/search/', GlobalSearchAPIView.as_view(), name='global-search')
+    path('api/search/', GlobalSearchAPIView.as_view(), name='global-search'),
+
+    path('api/time-search/', TimeSearchAPIView.as_view(), name='time-search'),
 ]
