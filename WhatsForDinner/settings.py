ffe63bd74e0f4391ba27e58874175b14a69b60fa
mariami57 <mmitova3@gmail.com>
2026-01-08 10:10:46 +0200

    made some small settings updates concerning re-deployment

--- WhatsForDinner/settings.py
+++ WhatsForDinner/settings.py
@@ -95,13 +95,9 @@ WSGI_APPLICATION = 'WhatsForDinner.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
